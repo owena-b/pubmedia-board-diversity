252cd0adf2cfb29e63586fe052a830c3dfb689d4
oa8828a <oa8828a@american.edu>
2024-05-31 13:43:12 -0400

    iterative; raises error for tribal entities; correctly writes csv instead of starting over every iteration; reads in csv of links as list for iteration

--- scraper.py
+++ scraper.py
@@ -3,7 +3,13 @@ import csv
 from bs4 import BeautifulSoup
 from fixes import state_fixes
 
-URL = 'https://enterpriseefiling.fcc.gov/dataentry/public/form323/draftCopyForm323.html?displayType=html&appKey=25076ff38bd02040018bd90eb5de2332&id=25076ff38bd02040018bd90eb5de2332&goBack=N'
+URLS_LIST = []
+
+with open('Grantee_Ownership_Report_Links.csv', newline='') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        URLS_LIST.append(row[0])
+    URLS_LIST.pop(0)
 
 CSV_FILE = 'pubmedia-board-members-data.csv'
 
@@ -18,52 +24,55 @@ CSV_HEADERS = [
     'Voting Interest'
 ]
 
-r = requests.get(URL)
-
-sections = BeautifulSoup(r.text, 'html.parser').find_all('section')
-
-# Takes the licensee's name from the first table at the top of the page.
-licensee = str(sections[0].find_all('table')[0].find_all('td')[1].text.strip())
-
-# Translates the state abbreviations into their full names.
-state = state_fixes.get(str(sections[0].find_all('table')[1].find_all('td')[2].text.strip()))
-
-# The 'tables' variable excludes the last table, which is a questionnaire about voting and equity interests.
-tables = sections[6].find_all('table')[:-1]
-
 with open(CSV_FILE, 'w', newline='', encoding='utf-8') as outfile:
     writer = csv.DictWriter(outfile, fieldnames=CSV_HEADERS)
     writer.writeheader()
+    for URL in URLS_LIST:
+        r = requests.get(URL)
 
-    for table in tables:
-        tbody = table.find('tbody')
-        row = tbody.find_all('tr')
+        sections = BeautifulSoup(r.text, 'html.parser').find_all('section')
 
-        # The first (and sometimes second) table in the section is the licensee or its parent entity. By identifying
-        # the type of table, the program skips tables for entities and only collects data on humans.
-        if row[1].find_all('td')[0].text.strip() == 'Entity Name':
-            continue
+        # Takes the licensee's name from the first table at the top of the page.
+        licensee = str(sections[0].find_all('table')[0].find_all('td')[1].text.strip())
 
-        name = row[1].find_all('td')[1].text.strip()
+        # Translates the state abbreviations into their full names.
+        state = state_fixes.get(str(sections[0].find_all('table')[1].find_all('td')[2].text.strip()))
 
-        occupation = row[11].find_all('td')[1].text.strip()
+        # The 'tables' variable excludes the last table, which is a questionnaire about voting and equity interests.
+        tables = sections[6].find_all('table')[:-1]
 
-        gender = row[15].find_all('td')[1].text.strip()
+        for table in tables:
+            tbody = table.find('tbody')
+            row = tbody.find_all('tr')
 
-        ethnicity = row[16].find_all('td')[1].text.strip()
+            # The first (and sometimes second) table in the section is the licensee or its parent entity. By identifying
+            # the type of table, the program skips tables for entities and only collects data on humans.
+            if row[1].find_all('td')[0].text.strip() == 'Entity Name':
+                # Checks for Tribal entities, which are excluded from our analysis.
+                if row[11].find_all('td')[1].text.strip() == 'Interest holder is a Tribal nation or Tribal entity':
+                    raise ValueError(f"Tribal entity found! Check {licensee}")
+                continue
 
-        race = row[17].find_all('td')[1].text.strip()
+            name = row[1].find_all('td')[1].text.strip()
 
-        voting = row[18].find_all('td')[2].text.strip()
+            occupation = row[11].find_all('td')[1].text.strip()
 
-        # Write the row
-        writer.writerow({
-            'Licensee': licensee,
-            'State': state,
-            'Name': name,
-            'Occupation': occupation,
-            'Gender': gender,
-            'Ethnicity': ethnicity,
-            'Race': race,
-            'Voting Interest': voting
-        })
+            gender = row[15].find_all('td')[1].text.strip()
+
+            ethnicity = row[16].find_all('td')[1].text.strip()
+
+            race = row[17].find_all('td')[1].text.strip()
+
+            voting = row[18].find_all('td')[2].text.strip()
+
+            # Write the row
+            writer.writerow({
+                'Licensee': licensee,
+                'State': state,
+                'Name': name,
+                'Occupation': occupation,
+                'Gender': gender,
+                'Ethnicity': ethnicity,
+                'Race': race,
+                'Voting Interest': voting
+            })
