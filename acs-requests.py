b4248ebc0752f195809d4579c68cbf808b6e4adc
oa8828a <oa8828a@american.edu>
2024-07-29 15:12:52 -0400

    All American Community Survey code and CSVs

--- acs-requests.py
+++ acs-requests.py
@@ -1,5 +1,4 @@
 import requests
-import pandas as pd
 
 # Replace with your actual API key
 api_key = '82d15734a7d6fa98f0968b97f19fdcf6052009b6'
@@ -12,27 +11,14 @@ fips_codes = {
 # Build the API request URL
 base_url = 'https://api.census.gov/data/2020/acs/acs5'
 params = {
-    'get': 'NAME,B02001_002E,B02001_003E,B02001_004E,B02001_005E,B02001_006E,B02001_007E,B02001_008E,B03001_003E',
-    'for': 'place:' + ','.join(fips_codes.values()),
+    'get': 'NAME,B02001_007E,B02001_008E,B02001_009E',
+    'for': 'place:38250',
+    'in': 'state:47',
     'key': api_key
 }
 
 # Make the API request
 response = requests.get(base_url, params=params)
-data = response.json()
+data = response.text
 
-# Convert the data to a pandas DataFrame
-columns = data[0]
-rows = data[1:]
-df = pd.DataFrame(rows, columns=columns)
-
-# Rename the columns for readability
-df.columns = ['City', 'White', 'Black or African American', 'American Indian and Alaska Native', 'Asian',
-              'Native Hawaiian and Other Pacific Islander', 'Some other race', 'Two or more races', 'Hispanic or Latino']
-
-# Convert data types to appropriate types
-for col in df.columns[1:]:
-    df[col] = df[col].astype(int)
-
-# Display the DataFrame
-print(df)
+print(data)
--- census-gets.py
+++ census-gets.py
@@ -1,15 +1,64 @@
+import csv
+
 from census import Census
-from us import states
 
-varsList = ['NAME', 'SUMLEVEL']
+inCSV = 'licensee_locs_2.csv'
+outCSV = 'city_demos_2.csv'
 
-bigList = [['place', '03000', '02', ''], ['cousub', '77255', '36', '007']]
+varsList = ['NAME', 'B01003_001E', 'B02001_002E', 'B02001_003E', 'B02001_004E', 'B02001_005E', 'B02001_006E',
+            'B02001_008E', 'B03003_003E']
+headers = ['Place', 'Total', 'White alone', 'Black or African American alone', 'American Indian and Alaska Native alone',
+           'Asian alone', 'Native Hawaiian and Other Pacific Islander alone', 'Multiracial', 'Hispanic or Latino']
+
+inList = []
+outList = []
+
+with open(inCSV, newline='') as inFile:
+    reader = csv.reader(inFile)
+    for row in reader:
+        inList.append(row)
+    inList.pop(0)
 
 c = Census(key='82d15734a7d6fa98f0968b97f19fdcf6052009b6', year=2022)
 
-for loc in bigList:
-    if loc[0] == 'place':
-        response = c.acs5.state_place(varsList, loc[2], loc[1])[0]
-    elif loc[0] == 'cousub':
-        response = c.acs5.state_county_subdivision(varsList, loc[2], loc[1], loc[3])
-    print(response)
+response = ''
+
+with open(outCSV, 'w', newline='', encoding='utf-8') as outFile:
+    writer = csv.DictWriter(outFile, fieldnames=headers)
+    writer.writeheader()
+    for loc in inList:
+        if loc[2] == 'ERROR':
+            continue
+        if loc[2] == 'place':
+            response = c.acs5.state_place(varsList, loc[4], loc[3])[0]
+        elif loc[2] == 'cousub':
+            response = c.acs5.state_county_subdivision(varsList, loc[4], loc[5], loc[3])[0]
+        elif loc[2] == 'state':
+            response = c.acs5.state(varsList, loc[4])[0]
+
+        if type(response) is dict and bool(response):  # check if it's a non-empty dictionary
+            if loc[2] != 'state':
+                name = loc[0] + ', ' + loc[1]
+            else:
+                name = "State of " + loc[1]
+            total = int(response.get('B01003_001E'))
+            white = int(response.get('B02001_002E'))
+            black = int(response.get('B02001_003E'))
+            native = int(response.get('B02001_004E'))
+            asian = int(response.get('B02001_005E'))
+            nhpi = int(response.get('B02001_006E'))
+            multiracial = int(response.get('B02001_008E'))
+            hispanic = int(response.get('B03003_003E'))
+
+            writer.writerow({
+                'Place': name,
+                'Total': total,
+                'White alone': white,
+                'Black or African American alone': black,
+                'American Indian and Alaska Native alone': native,
+                'Asian alone': asian,
+                'Native Hawaiian and Other Pacific Islander alone': nhpi,
+                'Multiracial': multiracial,
+                'Hispanic or Latino': hispanic
+            })
+
